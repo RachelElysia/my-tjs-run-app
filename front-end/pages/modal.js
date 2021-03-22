56bd329360ed8e2e6c15015406fdd51fa31ee5a0
Rachel Elysia Perkins <rachel.elysia.perkins@gmail.com>
2021-03-22 00:56:47 +0000

    Superfluous comment

--- front-end/pages/modal.js
+++ front-end/pages/modal.js
@@ -3,8 +3,6 @@ import styles from '../styles/Home.module.css'
 import Fade from 'react-reveal/fade'
 import modal from '../components/venmomodal.js';
 
-// self calling function... turn a function into an object and then call it with ()
-
 export default function Home(props) {
   return (
     <section id="page-container">
