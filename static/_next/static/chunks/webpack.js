28bf32c784aa4d6b4ef4701d3ebe9bf8496d3f82
Rachel Elysia Perkins <rachel.elysia.perkins@gmail.com>
2021-03-26 03:32:21 +0000

    Update static folder for deployment

--- static/_next/static/chunks/webpack.js
+++ static/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d9088eb2b5cf9733f5b4";
+/******/ 	var hotCurrentHash = "6197b6af769dd7406315";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
